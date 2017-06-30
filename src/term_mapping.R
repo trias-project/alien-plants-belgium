321ee4959a6175ceda2a42f6cf03c3723918c820
Peter Desmet <peter.desmet.work@gmail.com>
2017-07-01 00:49:26 +0200

    Create term_mapping as function (for lookup)

---
+++ src/term_mapping.R
@@ -0,0 +1,22 @@
+term_mapping <- function(lookup_table, term) {
+  # Filter lookup table on term (expected to be first column)
+  
+  # Unfortunately we cannot pass 'term' as a variable to 
+  # filter(lookup_table[[1]] == term)
+  # So, we need to create the condition as a string and pass to filter_()
+  condition <- paste0(colnames(lookup_table)[[1]], "==\"", term, "\"")
+  # e.g. 'term == "locationID"'
+  
+  lookup_table %>%
+    filter_(condition) -> term_lookup_table
+  
+  # Throw error if filter returns no results
+  if (nrow(term_lookup_table) == 0) stop(paste0("Term \"", term, "\" not found in first column of lookup table."))
+  
+  # Create list with new values (expected to be 3rd column) as values
+  term_map <- as.vector(term_lookup_table[[3]])
+  # And lookup values (expected to be 2nd column) as names
+  term_map <- setNames(term_map, term_lookup_table[[2]])
+  
+  return(term_map)
+}
